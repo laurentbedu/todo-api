5d696617dc4aef72b2a546bb03967dbd60d7235e
laurentbedu <bedulaurent@gmail.com>
2023-02-23 07:58:20 +0100

    correction TP 11

--- api/services/database.service.js
+++ api/services/database.service.js
@@ -40,23 +40,140 @@ async function selectAll(table) {
 }
 
 async function selectOne(table, id) {
-
+  const sql = `SELECT * FROM ${table} WHERE is_deleted = 0 AND id = ${id}`;
+  let resp;
+  await query(sql)
+    .then((data) => {
+      data = data.length == 1 ? data.pop() : null;
+      const result = data != null;
+      const message =
+        (result ? `the` : `NO`) +
+        ` row of table ${table} with id=${id} has been selected`;
+      resp = { data, result, message };
+    })
+    .catch((err) => {
+      resp = { data: null, result: false, message: err.message };
+    });
+  return resp;
 }
 
-async function createOne(table) {
-
+async function createOne(table, body) {
+  for (const key in body) {
+    if (typeof body[key] == "string")
+      body[key] = body[key].replace(/'/g, "\\'");
+  }
+  const keys = Object.keys(body).join(",");
+  const values = "'" + Object.values(body).join("','") + "'";
+  const sqlInsert = `INSERT INTO ${table} ( ${keys} ) VALUES ( ${values} )`;
+  let resp;
+  await query(sqlInsert)
+    .then(async (insertResult) => {
+      const { insertId } = insertResult;
+      const sqlSelect = `SELECT * FROM ${table} WHERE is_deleted = 0 AND id = ${insertId}`;
+      await query(sqlSelect)
+        .then((data) => {
+          data = data.length == 1 ? data.pop() : null;
+          const result = data != null && insertResult.affectedRows == 1;
+          const message =
+            (result ? `a` : `NO`) +
+            ` row with id=${insertId} has been inserted in table ${table}`;
+          resp = { data, result, message };
+        })
+        .catch((err) => {
+          resp = { data: null, result: false, message: err.message };
+        });
+    })
+    .catch((err) => {
+      resp = { data: null, result: false, message: err.message };
+    });
+  return resp;
 }
 
-async function updateOne(table, id) {
-
+async function updateOne(table, id, body) {
+    for (const key in body) {
+        if (key == "is_deleted") delete body[key];
+    
+        if (typeof body[key] == "string")
+          body[key] = body[key].replace(/'/g, "\\'");
+      }
+      const entries = Object.entries(body);
+      const values = entries
+        .map((entry) => {
+          const [key, value] = entry;
+          return `${key}='${value}'`;
+        })
+        .join(",");
+      const sqlUpdate = `UPDATE ${table} SET ${values} WHERE is_deleted = 0 AND id = ${id}`;
+      let resp;
+      await query(sqlUpdate)
+        .then(async (updateResult) => {
+          const sqlSelect = `SELECT * FROM ${table} WHERE is_deleted = 0 AND id = ${id}`;
+          await query(sqlSelect)
+            .then((data) => {
+              data = data.length == 1 ? data.pop() : null;
+              const result = data != null && updateResult.affectedRows == 1;
+              const message =
+                (result ? `the` : `NO`) +
+                ` row of table ${table} with id=${id} has been updated`;
+                resp = { data, result, message };
+            })
+            .catch((err) => {
+                resp = { data: null, result: false, message: err.message };
+            });
+        })
+        .catch((err) => {
+            resp = { data: null, result: false, message: err.message };
+        });
+        return resp;
 }
 
 async function softDeleteOne(table, id) {
-
+    const sqlUpdate = `UPDATE ${table} SET is_deleted = 1 WHERE is_deleted = 0 AND id = ${id}`;
+    let resp;
+    await query(sqlUpdate)
+      .then(async (updateResult) => {
+        const sqlSelect = `SELECT * FROM ${table} WHERE is_deleted = 1 AND id = ${id}`;
+        await query(sqlSelect)
+          .then((data) => {
+            data = data.length == 1 ? data.pop() : null;
+            const result = data != null && updateResult.affectedRows == 1;
+            const message =
+              (result ? `the` : `NO`) +
+              ` row of table ${table} with id=${id} has been softly deleted`;
+              resp = { data, result, message };
+          })
+          .catch((err) => {
+            resp = { data: null, result: false, message: err.message };
+          });
+      })
+      .catch((err) => {
+        resp = { data: null, result: false, message: err.message };
+      });
+      return resp;
 }
 
 async function hardDeleteOne(table, id) {
-
+    const sqlDelete = `DELETE FROM ${table} WHERE id = ${id}`;
+    let resp;
+    await query(sqlDelete)
+      .then(async (deleteResult) => {
+        const sqlSelect = `SELECT * FROM ${table} WHERE id = ${id}`;
+        await query(sqlSelect)
+          .then((data) => {
+            const result = data.length == 0 && deleteResult.affectedRows == 1;
+            const message =
+              (result ? `the` : `NO`) +
+              ` row of table ${table} with id=${id} has been hardly deleted`;
+              resp = { data, result, message };
+          })
+          .catch((err) => {
+            resp = { data: null, result: false, message: err.message };
+          });
+      })
+      .catch((err) => {
+        resp = { data: null, result: false, message: err.message };
+      });
+      return resp;
 }
 
 module.exports = {
